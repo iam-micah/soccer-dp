146eac3c89dd36c6bd2c4d36044c2c81872a1107
iam-micah <micahnettey@gmail.com>
2024-04-16 23:29:16 -0400

    sample query

--- proj.py
+++ proj.py
@@ -5,6 +5,16 @@ from snsql import Privacy
 from opendp.smartnoise.sql import PandasReader, PrivateReader
 from opendp.smartnoise.metadata import CollectionMetadata
 
+metadata = CollectionMetadata.from_file('metadata.yml')
 
 injuries_df = pd.read_csv('data/injuries.csv')
 players_df = pd.read_csv('data/players.csv')
+
+privacy = Privacy(epsilon=1.0, delta=0)
+reader = PandasReader(
+    metadata, {'injuries': injuries_df, 'players': players_df})
+private_reader = PrivateReader(reader, privacy)
+
+query = "SELECT injury_type, COUNT(*) AS injury_count FROM injuries GROUP BY injury_type"
+result = private_reader.execute(query)
+print(result)
